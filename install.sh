0f10fd4f7d0559f108354b0118192d2e17926e26
Erik Krieg <erik.c.krieg@gmail.com>
2022-09-17 01:24:46 -0400

    initial setup with nix and a few packages

---
+++ install.sh
@@ -0,0 +1,19 @@
+# Nix as a package manager
+if [ -z $(command -v nix-env) ]; then
+  curl -L https://nixos.org/nix/install | sh
+  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+else
+  echo "Nix already installed: $(nix-env --version)"
+fi
+
+nix-env -iA --dry-run \
+  nixpkgs.zsh \
+  nixpkgs.git \
+  nixpkgs.gum \
+  nixpkgs.stow \
+	nixpkgs.neovim \
+  nixpkgs.bat \
+  nixpkgs.delta
+
+stow packages/git
+
