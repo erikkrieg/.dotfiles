d9b2a3171455a63aa9d8e4295af561cdd76c706d
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-09-17 14:33:14 -0400

    makes sure zsh installed by nix is used over system default zsh

--- install.sh
+++ install.sh
@@ -30,5 +30,8 @@ stow -d ./packages -t "${HOME}/.config" neovim
 stow -d "$(nix-env -q --installed iterm2 --out-path --no-name)" -t /Applications Applications
 stow -d "$(nix-env -q --installed alacritty --out-path --no-name)" -t /Applications Applications
 
+command -v zsh | sudo tee -a /etc/shell  # Use zsh as login shell
+sudo chsh -s "$(which zsh)" $USER        # Use zsh (installed by nix) as default shell
+
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 ~/.local/share/nvim/site/pack/packer/start/cmp-tabnine/install.sh
