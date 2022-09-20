43cf42e37d3db8ae3827cdc7c27b6eb38fdc7025
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-09-20 00:17:35 -0400

    adds second color scheme option for prompt (created by Anne)

--- packages/zsh/.config/zsh/prompt.zsh
+++ packages/zsh/.config/zsh/prompt.zsh
@@ -8,10 +8,15 @@ function prompt_git() {
   fi
 }
 
-P_VIM_MODE='%F{84}${ZVM_MODE}%f '
-P_DIR='%F{39}%1~%f '
-P_GIT='%F{240}$(prompt_git)%f'
-P_CARET='%F{201}❯%f '
+typeset -A SPRING_COLOR=([vi]=84 [dir]=39 [git]=240 [caret]=201)
+typeset -A FALL_COLOR=([vi]=214 [dir]=194 [git]=133 [caret]=166)
+typeset -A C
+set -A C ${(kv)SPRING_COLOR}
+
+P_VIM_MODE='%F{$C[vi]}${ZVM_MODE}%f '
+P_DIR='%F{$C[dir]}%1~%f '
+P_GIT='%F{$C[git]}$(prompt_git)%f'
+P_CARET='%F{$C[caret]}❯%f '
 
 PROMPT="${P_VIM_MODE}${P_DIR}${P_GIT}${P_CARET}"
 
