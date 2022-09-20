e8b0b5208956e71bfc86e632b35e9e05182e9068
Erik Krieg <10244162+erikkrieg@users.noreply.github.com>
2022-09-19 23:35:00 -0400

    removes my own vi mode settings because they are handled by a plugin now

--- packages/zsh/.config/zsh/nvim-mode.zsh
+++ packages/zsh/.config/zsh/nvim-mode.zsh
@@ -2,14 +2,6 @@
 export EDITOR='nvim'
 export VISUAL='nvim'
 
-# Enables vi-mode for editing command line (uses default editor set above).
-bindkey -v
-export KEYTIMEOUT=1
-
-autoload edit-command-line
-zle -N edit-command-line
-bindkey '^v' edit-command-line
-
 # There is also a home manager way to source this that I should investigate.
 zinit ice depth=1; zinit light jeffreytse/zsh-vi-mode
 
